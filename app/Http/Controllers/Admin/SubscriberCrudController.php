ee87f167519eef617494e227cc359d8fa0ba2091
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-10-26 10:40:09 -0300

    [Crud] update show field tipe

--- app/Http/Controllers/Admin/SubscriberCrudController.php
+++ app/Http/Controllers/Admin/SubscriberCrudController.php
@@ -21,7 +21,7 @@ class SubscriberCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -33,7 +33,7 @@ class SubscriberCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
@@ -41,18 +41,28 @@ class SubscriberCrudController extends CrudController
     {
         CRUD::column('name');
         CRUD::column('phone');
-        CRUD::column('status');
+        CRUD::addColumn([
+            'name'  =>  'status',
+            'label' =>  'Status',
+            'type'  =>  'radio',
+            'options' => [
+                1 => 'Ativo',
+                0 => 'Inativo'
+            ],
+            'inline' => true,
+            'default' => 1
+        ]);
 
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
          */
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -62,18 +72,28 @@ class SubscriberCrudController extends CrudController
 
         CRUD::field('name');
         CRUD::field('phone');
-        CRUD::field('status');
+        CRUD::addField([
+            'name'  =>  'status',
+            'label' =>  'Status',
+            'type'  =>  'radio',
+            'options' => [
+                1 => 'Ativo',
+                0 => 'Inativo'
+            ],
+            'inline' => true,
+            'default' => 1
+        ]);
 
         /**
          * Fields can be defined using the fluent syntax or array syntax:
          * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
          */
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
